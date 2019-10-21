892afe8208169ceea6437fbd05b73964179c281f
DALNA <kurumahunokamisama@gmail.com>
2019-10-21 11:59:51 +0900

    ちょっとアニメーターの復習したかも

--- TimeAttacker/Assets/Scripts/Playermove.cs
+++ TimeAttacker/Assets/Scripts/Playermove.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Playermove : MonoBehaviour
 {
+    private Animator animator;
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
     }
 }
