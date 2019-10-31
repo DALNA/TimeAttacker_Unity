2ce7154d75a707ff42ee9be7ef79dc74b0042062
DALNA <kurumahunokamisama@gmail.com>
2019-10-31 14:51:16 +0900

    教科書の復習を応用

---
+++ TimeAttacker/Assets/Scripts/Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+
+    public float deleteTime = 1.3f;
+    void Start()
+    {
+        Destroy(gameObject, deleteTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- TimeAttacker/Assets/Scripts/Playermove.cs
+++ TimeAttacker/Assets/Scripts/Playermove.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Playermove : MonoBehaviour
 {
     private Animator animator;
-    public float speed;
+    public float speed = 5f; //キャラクタースピード
+    public float power = 1000f; //投げるパワー
+    public GameObject fireball;
+    public Transform ballspawn;
     Rigidbody2D rb2d;
 
     void Start()
@@ -13,14 +16,24 @@ public class Playermove : MonoBehaviour
         animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
     }
-    
+
     void Update()
     {
+        transform.Translate(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, 0);
 
-
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //クリックした時、ボールを投げる
+        {
             animator.SetBool("OnClick", true);
+            BallThrow ();
+        }
         else
             animator.SetBool("OnClick", false);
     }
+
+    void BallThrow()
+    {
+        GameObject newBullet = Instantiate(fireball, ballspawn.position, Quaternion.identity) as GameObject;
+        newBullet.GetComponent<Rigidbody2D>().AddForce(Vector3.up * power);
+    }
+
 }
