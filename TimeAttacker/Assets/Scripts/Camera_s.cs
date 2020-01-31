3563551b5604873b356f3cc52fe3cf854ca4b1d8
DALNA <kurumahunokamisama@gmail.com>
2020-01-31 18:10:41 +0900

    コミットしました。

---
+++ TimeAttacker/Assets/Scripts/Camera_s.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_s : MonoBehaviour
+{
+
+    public GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+
+        transform.position = new Vector3(playerPos.x, 0, -10);
+    }
+}
